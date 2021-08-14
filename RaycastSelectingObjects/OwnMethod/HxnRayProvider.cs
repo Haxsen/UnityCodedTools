cd226d8eca82e8facf53a9ff4f8834895d9fdf45
haxsen <hassaan.ali.lel@gmail.com>
2021-08-14 22:09:55 +0500

    Added Raycast Selecting Objects

---
+++ RaycastSelectingObjects/OwnMethod/HxnRayProvider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _Practice.SOLID.OwnMethod
+{
+    public class HxnRayProvider
+    {
+        public HxnRayProvider() {}
+        
+        public Ray CreateRayFromMousePosition()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
